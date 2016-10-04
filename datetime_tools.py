fcbb32b62b15d9a9ebd15647e5c0755226964572
max <mh@stealth.at>
2016-10-04 16:02:00 +0200

    added get_utc_datetime_average

--- datetime_tools.py
+++ datetime_tools.py
@@ -3,7 +3,7 @@ import dateutil.parser
 import pytz
 
 
-def get_utc_datetime_from_string(str):
+def get_utc_datetime_from_string_with_timezone(str):
     dt = dateutil.parser.parse(str)
     return dt.astimezone(pytz.utc)
 
@@ -15,3 +15,10 @@ def get_utc_datetime_from_unaware_str(str):
     dt = dateutil.parser.parse(str)
     return pytz.utc.localize(dt)
 
+
+def get_utc_datetime_average( dt1, dt2 ):
+    avg = datetime.datetime.utcfromtimestamp( (dt1.timestamp() + dt2.timestamp() ) / 2 )
+    avg = pytz.utc.localize(avg)
+    # avg = tz
+    # avg.astimezone(dt1.tzname())
+    return avg
